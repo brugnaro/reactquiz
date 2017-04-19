eab63642bf9230675cf516c73ecbb33c7ee1769f
marcos rodrigues <psicodelicoarts@hotmail.com>
2017-04-19 03:29:44 -0300

    criacao do layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,98 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import QuestionList from './quiz/QuestionList.jsx'
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            questions: [
+                {
+                    id: 1,
+                    text: 'Whats is your name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Michael'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Brad'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Steven'
+                        }
+                    ],
+                    correct: 'b'
+                },
+                {
+                    id: 2,
+                    text: 'Whats is your mothers name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Sara'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Sue'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Donna'
+                        }
+                    ],
+                    correct: 'c'
+                },
+                {
+                    id: 3,
+                    text: 'Whats is your fathers name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Boby'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Hary'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Wayne'
+                        }
+                    ],
+                    correct: 'c'
+                },
+                {
+                    id: 4,
+                    text: 'Whats is your friends name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'John'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Paul'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Jose'
+                        }
+                    ],
+                    correct: 'a'
+                }
+            ],
+            score: 0,
+            current: 1
+        }
+    }
+
     render(){
         return(
             <div>
-                App
+                <QuestionList {...this.state} />
             </div>
         )
     }
